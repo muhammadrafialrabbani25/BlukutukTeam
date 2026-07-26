077e8cad68e404da7ddf1596df67b2f7139f9cf3
muhammadrafialrabbani25 <muhammad.rafi.tif425@polban.ac.id>
2026-07-26 19:09:59 +0700

    nyoba asset imam

--- script/player.gd
+++ script/player.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
-
 const SPEED = 250.0
 const JUMP_VELOCITY = -600.0
+@onready var player_ui: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
+	
+#	if velocity.x !=0:
+#		player_ui.animation = "run"
+#	else: 
+#		player_ui.animation = "idle"
+	
+	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -18,6 +26,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("jalan_kiri", "jalan_kanan")
 	if direction:
+		player_ui.flip_h = direction > 0
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
